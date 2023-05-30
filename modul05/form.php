a348b484b95dec3de26bce2a4dfe4b4fc24616d1
Risyofi <95907287+Risyofi@users.noreply.github.com>
2023-05-30 13:50:54 +0700

    Update form.php

--- modul05/form.php
+++ modul05/form.php
@@ -1,91 +1,89 @@
-<html>
-    <head>
-        <title>Data Mahasiswa</title>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Barang</title>
     <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'informatika');
+    $conn= mysqli_connect('localhost', 'root', '','stok_gudang')
     ?>
-    <body>
-        <center>
-            <h3>Masukkan Data Mahasiswa :</h3>
-            <form action="form.php" method='POST'>
-                <table border='0' width='30%'>
-                    <tr>
-                        <td width='25%'>NIM</td>
-                        <td width='5%'>:</td>
-                        <td width='65%'><input type="text" name='nim' size='10'></td>
-                    </tr>
-                    <tr>
-                        <td width='25%'>Nama</td>
-                        <td width='5%'>:</td>
-                        <td width='65%'><input type="text" name='nama' size='10'></td>
-                    </tr>
-                    <tr>
-                        <td width='25%'>Kelas</td>
-                        <td width='5%'>:</td>
-                        <td width='65%'>
-                            <input type="radio" value='A' checked name='kelas'>A
-                            <input type="radio" value='B' checked name='kelas'>B
-                            <input type="radio" value='C' checked name='kelas'>C
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width='25%'>Alamat</td>
-                        <td width='5%'>:</td>
-                        <td width='65%'><input type="text" name='alamat' size='40'></td>
-                    </tr>
-                </table>
-                <input type="submit" value='Masukkan' name='submit'>
-            </form>
-            
-            <?php
-            error_reporting(E_ALL ^ E_NOTICE);
-            $nim = $_POST['nim'];
-            $nama = $_POST['nama'];
-            $kelas = $_POST['kelas'];
-            $alamat = $_POST['alamat'];
-            $submit = $_POST['submit'];
-            $input = "insert into mahasiswa (nim, nama, kelas, alamat) 
-            values ('$nim', '$nama', '$kelas', '$alamat')";
-            if($submit) {
-                if($nim=='') {
-                    echo "</br>NIM tidak boleh kosong, diisi dulu";
-                }elseif($nama==''){
-                    echo "</br>Nama tidak boleh kosong, diisi dulu";
-                }elseif($alamat==''){
-                    echo "</br>Alamat tidak boleh kosong, diisi dulu";
-                }else{
-                    mysqli_query($conn, $input);
-                    echo"</br>Data berhasil dimasukkan";
-                }
-            }
-            ?>
-        
-        <hr>
-        <H3>Data Mahasiswa</H3>
-        <table border='1' width='50%'>
-            <tr>
-                <td align='center' width='20%'><b>NIM</b></td>
-                <td align='center' width='30%'><b>Nama</b></td>
-                <td align='center' width='10%'><b>Kelas</b></td>
-                <td align='center' width='30%'><b>Alamat</b></td>
-                <td align='center' width='10%'><b>Keterangan</b></td>
-            </tr>
-            <?php
-            $cari="select * from mahasiswa order by nim";
-            $hasil_cari=mysqli_query($conn, $cari);
-            while($data=mysqli_fetch_row($hasil_cari)) {
-                echo"
+</head>
+<body>
+    <center>
+        <h3>masukkan data :</h3>
+        <table>
+            <form action="" method="post">
                 <tr>
-                <td width='20%'>$data[0]</td>
-                <td width='30%'>$data[1]</td>
-                <td width='10%'>$data[2]</td>
-                <td width='30%'>$data[3]</td>
-                <td><a href='edit.php?nim=$data[0]'>Ubah</a></td>
-                </tr>";
-            }
-            ?> 
+                <td>kdoe barang</td>
+                <td><input type="text" name="kb"></td>
+                </tr>
+                <tr>
+                <td>nama barang</td>
+                <td><input type="text" name="nb"></td>
+                </tr>
+                <tr>
+                <td>GUDANG :</td>
+                <td>
+                <?php echo "<select name='kg' >"; ?>
+                <?php
+                    $sql = "select * from gudang";
+                    $retval = mysqli_query($conn,$sql);
+                    while($row = mysqli_fetch_array($retval)){
+                        echo "<option value='$row[kode_gudang]'>$row[nama_gudang]</option>";
+                    }
+                ?>
+                <?php echo "</select>"; ?>
+                </td>
+                </tr>
+                <tr>
+                    <input type="submit" name='submit' value="Masukkan">
+
+                </tr>
+            </form>
         </table>
-        </center>            
-    </body>
+        <br><br>
+        <hr>
+<h3>Data Barang</h3>
+<table border='1' width='50%'>
+    <tr>
+        <td>kode barang</td>
+        <td>nama barang</td>
+        <td>lokasi</td>
+    </tr>
+    <!-- tampilkan data -->
+<?php
+$cari= "SELECT kode_barang, nama_barang, lokasi from barang, gudang where barang.kode_gudang=gudang.kode_gudang";
+$hasil_cari=mysqli_query($conn,$cari);
+while($data=mysqli_fetch_row($hasil_cari)){ echo"
+<tr>
+<td width='20%'>$data[0]</td>
+<td width='30%'>$data[1]</td>
+<td width='10%'>$data[2]</td>
+</tr>";
+}
+?>
+                </table>
+    </center>
+    
+</body>
 </html>
+
+
+<!-- kirim barang -->
+<?php
+error_reporting(0);
+$kb = $_POST["kb"];
+$nb = $_POST["nb"];
+$kg = $_POST["kg"];
+$submit = $_POST["submit"];
+echo "$kb $nb $kg";
+$input= "insert into barang (kode_barang, nama_barang, kode_gudang) values ('$kb', '$nb', '$kg') ";
+if($submit){
+    mysqli_query($conn,$input);
+echo"</br>Data berhasil dimasukkan";
+header("Refresh:0; url=index.php");
+}
+?>
+
+
