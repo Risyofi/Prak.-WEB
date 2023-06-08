0ba1af930e234d7b9ea109650b0486e618e7eb65
Risyofi <95907287+Risyofi@users.noreply.github.com>
2023-06-08 21:26:28 +0700

    Update login.php

--- modul09/Tugas/login.php
+++ modul09/Tugas/login.php
@@ -8,15 +8,16 @@
   <div class="container">
     <form method="post" action="proses_login.php">
       <div class="form-group">
-        <label for="username">Username:</label>
+        <label for="username">Username</label>
         <input type="text" id="username" name="username">
       </div>
       <div class="form-group">
-        <label for="password">Password:</label>
+        <label for="password">Password</label>
         <input type="password" id="password" name="password">
       </div>
       <div class="form-group">
         <input type="submit" name="submit" value="Submit">
+        <p>Don't have an account? <a href="sign_up.php">Sign Up</a></p>
       </div>
     </form>
   </div>
