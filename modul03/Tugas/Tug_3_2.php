432622233bf951b32a270aa763b75d84d1b92206
Risyofi <95907287+Risyofi@users.noreply.github.com>
2024-03-16 13:06:22 +0700

    Update Tug_3_2.php

--- modul03/Tugas/Tug_3_2.php
+++ modul03/Tugas/Tug_3_2.php
@@ -1,28 +1,26 @@
 <html>
-    <head>
-        <title>Angka Ganjil atau Genap</title>
-    </head>
+<head>
+    <title>Angka Ganjil atau Genap</title>
+</head>
 
-    <body>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <label for="angka">Masukkan angka : </label>
-            <input type="number" name="angka" id="angka">
-            <br><br>
-            <input type="submit" value="kirim" name="submit">
-        </form>
+<body>
+    <form action="tugas_3_2.php" method="POST">
+        <label for="angka">Masukkan angka : </label>
+        <input type="number" name="angka" id="angka">
+        <br><br>
+        <input type="submit" value="kirim" name="submit">
+    </form>
 
-        <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $angka = $_POST['angka'];
-            $submit = $_POST['submit'];
-            if($submit){
-                if($angka % 2 == 0){
-                    echo "$angka adalah bilangan genap";
-                } else{
-                    echo "$angka adalah bilangan ganjil";
-                }
-            }
+    <?php
+    error_reporting(E_ALL^E_NOTICE);
+    if(isset($_POST['submit'])) {
+        $angka = intval($_POST['angka']);
+        if($angka % 2 == 0){
+            echo "$angka adalah bilangan genap";
+        } else{
+            echo "$angka adalah bilangan ganjil";
         }
-        ?>
-    </body>
+    }
+    ?>
+</body>
 </html>
