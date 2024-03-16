2eb252024b9464f5f133e8943970dafc50554220
Risyofi <95907287+Risyofi@users.noreply.github.com>
2024-03-16 13:07:24 +0700

    Update Tug_3_1.php

--- modul03/Tugas/Tug_3_1.php
+++ modul03/Tugas/Tug_3_1.php
@@ -1,32 +1,28 @@
 <html>
-    <head>
-        <title>Program Penjumlahan</title>
-    </head>
+<head>
+    <title>Program Penjumlahan</title>
+</head>
 
-    <body>
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <label for="nilaiA">Nilai A adalah : </label>
-            <input type="number" id="nilaiA" name="nilaiA">
-            <br><br>
-            <label for="nilaiB">Nilai B adalah : </label>
-            <input type="number" id="nilaiB" name="nilaiB">
-            <br><br>
-            <input type="submit" value="Jumlahkan" name="submit">
-        </form>
-        <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $nilaiA = $_POST['nilaiA'];
-            $nilaiB = $_POST['nilaiB'];
-            $jumlahkan = $_POST['submit'];
-            if($jumlahkan){
-                $sum = $nilaiA + $nilaiB;
-                echo "Nilai A adalah $nilaiA </br>";
-                echo "Nilai B adalah $nilaiB </br>";
-                echo "Jadi Nilai A ditambah Nilai B adalah $sum";
-            }
-        }
-        ?>
-    </body>
+<body>
+    <form method="POST" action="tugas_3_1.php">
+        <label for="nilaiA">Nilai A adalah : </label>
+        <input type="number" id="nilaiA" name="nilaiA">
+        <br><br>
+        <label for="nilaiB">Nilai B adalah : </label>
+        <input type="number" id="nilaiB" name="nilaiB">
+        <br><br>
+        <input type="submit" value="Jumlahkan" name="submit">
+    </form>
+    <?php
+    error_reporting(E_ALL^E_NOTICE);
+    if(isset($_POST['submit'])) {
+        $nilaiA = intval($_POST['nilaiA']);
+        $nilaiB = intval($_POST['nilaiB']);
+        $sum = $nilaiA + $nilaiB;
+        echo "Nilai A adalah $nilaiA </br>";
+        echo "Nilai B adalah $nilaiB </br>";
+        echo "Jadi Nilai A ditambah Nilai B adalah $sum";
+    }
+    ?>
+</body>
 </html>
-
-
